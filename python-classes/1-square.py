31b5e0dc75393e8241d1bb4adbd28b2bbc9c8ea9
BryceKnight16 <6373@holbertonstudents.com>
2023-06-13 02:26:59 -0700

    creating a private instance of size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 
+"""Creating a module called 1-sqaure
+
+"""
+
+
 class Square:
+    """ Creating a private instance of the attribute size
+
+
+    """
     def __init__(self, __size):
         self.__size = __size
