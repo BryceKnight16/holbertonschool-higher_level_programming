6cafc2392ad46e56a0b58b37a655912cb63c7ad2
BryceKnight16 <6373@holbertonstudents.com>
2023-06-14 21:08:17 -0700

    using getters and setters for getting private attriubutes

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Creating a module called 4-sqaure
+
+"""
+
+
+class Square:
+    """ Initializes a Square instance.
+
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
