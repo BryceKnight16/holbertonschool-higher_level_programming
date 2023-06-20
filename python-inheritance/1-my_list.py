aba6255bf4ee7e088a042656413611cd3dc93e62
BryceKnight16 <6373@holbertonstudents.com>
2023-06-19 22:20:27 -0700

    fixing

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -11,6 +11,6 @@ class MyList(list):
     '''
     def print_sorted(self):
        '''
-        Function that print the sorted list.
-        '''
-        print(sorted(self))
+       Function that print the sorted list.
+       '''
+       print(sorted(self))
