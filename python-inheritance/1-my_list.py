782c6ef239ad51e97eca5b91a20a71d27df53ac8
BryceKnight16 <6373@holbertonstudents.com>
2023-06-19 22:04:15 -0700

    i dont know why it isnt wroking

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-"""
-    Module that including a class.
-"""
+'''
+    Creating a class called MyList that inherits from the class list
+'''
 
 
 class MyList(list):
-    """
+    '''
     A class that inheritance from built-in class list
-    """
+    '''
     def print_sorted(self):
-        """Function that print the sorted list.
-        """
+       '''
+        Function that print the sorted list.
+        '''
         print(sorted(self))
