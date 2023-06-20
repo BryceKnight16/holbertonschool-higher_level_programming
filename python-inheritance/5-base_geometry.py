4c46740e7a6fced7dbd30fb968129b16095c191f
BryceKnight16 <6373@holbertonstudents.com>
2023-06-19 23:12:13 -0700

    empty class called base_geometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+Creating module for an empty class called Basegeometry
+'''
+
+
+class Basegeomtry:
+    '''
+    Creating empty class called Basegeometry
+    '''
+    pass
