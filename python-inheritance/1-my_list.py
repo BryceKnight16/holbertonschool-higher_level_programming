b1029e49676739b01a8c3513534ded60cd8e3b15
BryceKnight16 <6373@holbertonstudents.com>
2023-06-19 21:36:35 -0700

    testing my_list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+
+"""
+    Module that including a class.
+"""
+
 
 class MyList(list):
+    """
+    A class that inheritance from built-in class list
+    """
     def print_sorted(self):
+        """Function that print the sorted list.
+        """
         sorted_list = sorted(self)
-        print (sorted_list)
+        print(sorted_listed)
