328dca84c0014bbb4650213706b87c94639ee797
BryceKnight16 <6373@holbertonstudents.com>
2023-06-22 01:17:05 -0700

    fixing doc string comments

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
+'''
+A module 0-readfile made it read files and print tot he standard
+Output
+'''
+
 
 def read_file(filename=""):
+    '''
+    Read file adn print to stdout
+
+    Args:
+    filename(str): Name of file to be read
+
+    Returns:
+    None
+
+    '''
+
     with open('my_file_0.txt', encoding="utf-8") as f:
         for line in f:
             print(line, end='')
