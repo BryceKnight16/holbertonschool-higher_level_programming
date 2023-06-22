33e9d0cba36bcdc37154e2f786216903a1980743
BryceKnight16 <6373@holbertonstudents.com>
2023-06-22 01:48:40 -0700

    writing in a txt file with the write method

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def write_file(filename="", text=""):
+    with open(filename, 'r+', encoding="utf-8") as f:
+        written_file= f.write(text)
+        print(written_file, end="")
