f427c8badb76bf57155f4d68ea4b1d0becdf526f
BryceKnight16 <6373@holbertonstudents.com>
2023-06-22 01:23:15 -0700

    i had no f read in it i just looped and printed which isnt correct

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -18,5 +18,5 @@ def read_file(filename=""):
     '''
 
     with open('my_file_0.txt', encoding="utf-8") as f:
-        for line in f:
-            print(line, end='')
+        read_file = f.read()
+        print(read_file, end='')
