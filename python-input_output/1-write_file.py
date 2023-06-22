14e6923b08e3d62053eb0155cde94a65500c9cb1
BryceKnight16 <6373@holbertonstudents.com>
2023-06-22 01:53:54 -0700

    changing the ending statement to a return instead of a print

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+'''
+Module that has a write file funciton
+'''
+
 
 def write_file(filename="", text=""):
+    '''
+    a function that write the text in the filename
+    '''
+
     with open(filename, 'r+', encoding="utf-8") as f:
         written_file= f.write(text)
         return written_file
