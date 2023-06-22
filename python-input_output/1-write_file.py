0afb0bf059c5318be9a4679a3fc8a259b9036067
Bryce Knight <124347057+BryceKnight16@users.noreply.github.com>
2023-06-22 19:43:21 +1000

    adding more comments to docstring

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,6 +7,13 @@ Module that has a write file funciton
 def write_file(filename="", text=""):
     '''
     a function that write the text in the filename
+
+    Args:
+    filename(str): the filename to be written in 
+    text(str): the text to be written in the file
+
+    Return:
+    the number of chars in written_file
     '''
 
     with open(filename, 'w', encoding="utf-8") as f:
