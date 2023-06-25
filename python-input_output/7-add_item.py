c58bdc92550a9d5eed2d9a5509017a2077c0275a
BryceKnight16 <6373@holbertonstudents.com>
2023-06-25 04:12:04 -0700

    adding item to from args to the python list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,8 +8,19 @@ import sys
 import json
 import os.path
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+def save_to_json_file(my_obj, filename):
+     '''
+     Function that writes an Object to a text file,
+     using a JSON representation
+     '''
+     with open(filename, 'w', encoding="utf-8") as f:
+         f.write(json.dumps(my_obj))
+
+def load_from_json_file(filename):
+    """Create object from JSON file"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
+
 add_item = "add_item.json"
 
 if os.path.isfile(add_item):
