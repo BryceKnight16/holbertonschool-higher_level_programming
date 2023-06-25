ddbc0ef96b120843000ba2b6ddf56e3f7c44aef6
BryceKnight16 <6373@holbertonstudents.com>
2023-06-25 04:07:50 -0700

    adding item to from args to the python list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,19 +5,16 @@ and then save them to a file
 '''
 
 import sys
+import json
+import os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+add_item = "add_item.json"
 
-
-args = sys.argv
-
-try:
-    new_list = load_from_json_file("add_item.json")
-except:
-    new_list = []
-
-for arg in args[1:]:
-    new_list.append(arg)
-
-save_to_json_file(new_list, "add_item.json")
+if os.path.isfile(add_item):
+    obj = load_from_json_file(add_item)
+else:
+    obj = []
+obj.extend(sys.argv[1:])
+save_to_json_file(obj, add_item)
