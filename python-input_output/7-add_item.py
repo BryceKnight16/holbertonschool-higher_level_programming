6cc3952f55d5b2024b209fcfcd2c49ace2fd4b4f
BryceKnight16 <6373@holbertonstudents.com>
2023-06-25 04:18:41 -0700

    trying things

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,20 +5,10 @@ and then save them to a file
 '''
 
 import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def save_to_json_file(my_obj, filename):
-     '''
-     Function that writes an Object to a text file,
-     using a JSON representation
-     '''
-     with open(filename, 'w', encoding="utf-8") as f:
-         f.write(json.dumps(my_obj))
-
-def load_from_json_file(filename):
-    """Create object from JSON file"""
-    with open(filename, "r", encoding="utf-8") as f:
-        return json.load(f)
 
 args = sys.argv
 try:
