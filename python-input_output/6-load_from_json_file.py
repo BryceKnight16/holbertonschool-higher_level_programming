7b332a7eaea73ec7cbeae2e456ea1f3c412d3c00
BryceKnight16 <6373@holbertonstudents.com>
2023-06-25 04:15:40 -0700

    fixing

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -9,6 +9,5 @@ def load_from_json_file(filename):
      '''
      Function that creates an Object from a “JSON file”
      '''
-     with open(filename, 'r', encoding="utf-8") as f:
-        jfile = json.load(f)
-        return jfile
+     with open(filename, encoding="utf-8") as f:
+        return json.load(f)
