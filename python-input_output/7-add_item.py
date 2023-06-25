2a8fddc3ae59545571961732d8c75728aec7224e
BryceKnight16 <6373@holbertonstudents.com>
2023-06-25 04:25:00 -0700

    trying things

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,9 +8,6 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-
-
 try:
     load_from_json_file('add_item.json')
     file_exists = True
