2871bcb0fe2df71c85a37e9e53e53a6913694587
BryceKnight16 <6373@holbertonstudents.com>
2023-06-29 22:18:00 -0700

    using the update method and assigns an argument to each attribute

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -155,9 +155,35 @@ class Rectangle(Base):
         '''
         Print the Rectangle instance in stdout using the character #.
         '''
-        for i in range(self.height):
-            print("#" * self.width)
+        for blank_space in range(self.__y):
+            print()
+        for blank_space in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - " \
                f"{self.width}/{self.height}"
+    
+    def update(self, *args):
+        '''
+        Assign the arguments to each attribute in the specified order.
+
+        Args:
+            *args: The arguments to be assigned to the attributes.
+
+        Raises:
+            ValueError: If the number of arguments is less than 1 or more than 5.
+        '''
+        if len(args) < 1 or len(args) > 5:
+            raise ValueError("Invalid number of arguments")
+
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
