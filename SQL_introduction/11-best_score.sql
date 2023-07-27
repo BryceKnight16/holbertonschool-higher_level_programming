792619560d3ffbaf355dedabee591095197aa293
BryceKnight16 <6373@holbertonstudents.com>
2023-07-27 03:42:55 -0700

    all files

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- lists scores greater than 10 in second_table
+SELECT score, name FROM second_table
+       WHERE score>= 10
+       ORDER BY score DESC;
---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- script that updates bobs score in second table
+UPDATE second_table
+SET score = 10
+WHERE name = "Bob";
